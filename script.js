64441a65afa645ce8088d034b8934f8b1e449c49
Joseph Badlato <jbadlato@vt.edu>
2017-08-29 12:19:03 -0400

    Add search bar to jump to a location

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 var map;
 function initialize() {
-  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-  var myOptions = {
-    zoom: 4,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  map = new google.maps.Map(document.getElementById("map"), myOptions);
+	var myLatlng = new google.maps.LatLng(47.6062,-122.3321);
+	var myOptions = {
+    	zoom: 10,
+    	center: myLatlng,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP
+  	}
+  	map = new google.maps.Map(document.getElementById("map"), myOptions);
 
-  google.maps.event.addListener(map, 'click', function(event) {
-    placeMarker(event.latLng);
-  });
+  	google.maps.event.addListener(map, 'click', function(e) {
+    	placeMarker(e.latLng);
+  	});
+
+  	var card = document.getElementById('search_bar');
+  	var input = document.getElementById('text_input');
+
+  	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+  	var autocomplete = new google.maps.places.Autocomplete(input);
+
+  	autocomplete.bindTo('bounds', map);
+
+  	autocomplete.addListener('place_changed', function() {
+  		var place = autocomplete.getPlace();
+  		if(!place.geometry) {
+  			window.alert("No details available for input: '" + place.name + "'");
+  			return;
+  		}
+
+  		if (place.geometry.viewport) {
+  			map.fitBounds(place.geometry.viewport);
+  		} else {
+  			map.setCenter(place.geometry.location);
+  			map.setZoom(17);
+  		}
+  	});
 }
 
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 function placeMarker(location) {
   var marker = new google.maps.Marker({
       position: location, 
+      icon: {
+      	path: google.maps.SymbolPath.CIRCLE,
+      	scale: 1,
+      	strokeColor: "#00F",
+      	strokeWeight: 3.0
+      },
       map: map
   });
-
-  map.setCenter(location);
 }
