db233597583faae8f6bac1ffd9649d80ab026b24
Joseph Badlato <jbadlato@vt.edu>
2017-08-29 15:11:21 -0400

    Draw path between waypoints

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var map;
 function initialize() {
 	var myLatlng = new google.maps.LatLng(47.6062,-122.3321);
+	console.log(myLatlng.lng());
 	var myOptions = {
-    	zoom: 10,
+    	zoom: 17,
     	center: myLatlng,
     	mapTypeId: google.maps.MapTypeId.ROADMAP
   	}
   	map = new google.maps.Map(document.getElementById("map"), myOptions);
 
+  	// Set up directions objects:
   	var directionsService = new google.maps.DirectionsService();
+  	var directionsDisplay = new google.maps.DirectionsRenderer({
+  		suppressMarkers: true,
+  		preserveViewport: true
+  	});
+  	directionsDisplay.setMap(map);
+
+  	// Add listener to add markers to map where user clicks (snaps to nearest road):
   	google.maps.event.addListener(map, 'click', function(e) {
   		var request = {
   			origin: e.latLng,
@@ -18,22 +27,20 @@ function initialize() {
 
   		directionsService.route(request, function(response, status) {
   			if (status == google.maps.DirectionsStatus.OK) {
-  				placeMarker(response.routes[0].legs[0].start_location)
+  				placeMarker(response.routes[0].legs[0].start_location);
+  				calculateAndDisplayRoute(directionsService, directionsDisplay);
   			} else {
   				alert('Error placing marker');
   			}
   		})
   	});
 
+  	// Add listener for the autocomplete "jump to location" search bar:
   	var card = document.getElementById('search_bar');
   	var input = document.getElementById('text_input');
-
   	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
-
   	var autocomplete = new google.maps.places.Autocomplete(input);
-
   	autocomplete.bindTo('bounds', map);
-
   	autocomplete.addListener('place_changed', function() {
   		var place = autocomplete.getPlace();
   		if(!place.geometry) {
@@ -50,8 +57,9 @@ function initialize() {
   	});
 }
 
+var markers = []; // stores the marker locations
 function placeMarker(location) {
-  var marker = new google.maps.Marker({
+  	var marker = new google.maps.Marker({
       position: location, 
       icon: {
       	path: google.maps.SymbolPath.CIRCLE,
@@ -60,5 +68,35 @@ function placeMarker(location) {
       	strokeWeight: 3.0
       },
       map: map
-  });
+ 	 });
+  	markers.push({
+  		latitude: location.lat(),
+  		longitude: location.lng()
+  	});
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+	start = new google.maps.LatLng(markers[0].latitude, markers[0].longitude);
+	end = new google.maps.LatLng(markers[markers.length-1].latitude, markers[markers.length-1].longitude);
+	ways = [];
+	for (var i = 1; i < markers.length-1; i++) {
+		newWay = new google.maps.LatLng(markers[i].latitude, markers[i].longitude);
+		ways.push({
+			location: newWay,
+			stopover: true
+		});
+	}
+	directionsService.route({
+		origin: start,
+		destination: end,
+		waypoints: ways,
+		optimizeWaypoints: true,
+		travelMode: 'WALKING'
+	}, function(response, status) {
+		if (status === 'OK') {
+			directionsDisplay.setDirections(response);
+		} else {
+			alert('Directions request failed due to ' + status);
+		}
+	});
 }
